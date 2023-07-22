2c3ba7087d3058695227006f73873e1e416eb28c
vbwck <bbneogame1@gmail.com>
2023-07-22 20:58:13 +0300

    some changes in 'index_index_view.php'

--- view/pages/index_index_view.php
+++ view/pages/index_index_view.php
@@ -1,1 +1,13 @@
-<?php
+
+<h1>Hello this is index-view</h1>
+<img src="../../public/images/Logo-Booking.png" alt="booking-pic">
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam consequuntur cumque dic
+    ta dignissimos dolorem doloremque eligendi eum ex ipsam non nulla officiis, optio quam quia quisquam sequi unde. A
+    alias aliquam animi, as
+    pernatur assumenda beatae ea, esse expedita fugit, labore minus neque nesciunt officiis possimu
+    asi quibusdam ullam vero.
+</p>
+<div class="button">
+    <a href="/booking/index">go on</a>
+</div>
