9b0435a32cac4948c8d5e2e9944cb7b90b2b4f9d
vbwck <bbneogame1@gmail.com>
2023-07-22 20:42:56 +0300

    created "main_template.php" - content

--- view/templates/main_template.php
+++ view/templates/main_template.php
@@ -1,1 +1,21 @@
-<?php
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Booking</title>
+        <link rel="stylesheet" href="../../public/css/style.css">
+    </head>
+    <header>
+        <img src="#" alt="logo">
+        <h1>site name</h1>
+    </header>
+    <body>
+        <?php include_once '../view/pages/' . $page . '_view.php' ?>
+    </body>
+    <footer>
+        created by jan_team.
+    </footer>
+</html>
