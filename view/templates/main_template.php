783c15509b867ef71d5edde44b6cbe0043768c30
@mvolodimirovic15 <mvolodimirovic15@gmail.com>
2023-07-22 21:26:13 +0300

    Add header and footer

--- view/templates/main_template.php
+++ view/templates/main_template.php
@@ -8,14 +8,54 @@
         <title>Booking</title>
         <link rel="stylesheet" href="../../public/css/style.css">
     </head>
-    <header>
-        <img src="#" alt="logo">
-        <h1>site name</h1>
-    </header>
-    <body>
-        <?php include_once '../view/pages/' . $page . '_view.php' ?>
+    <body class="body">
+        <div class="l-wrapper">
+            <header class="header">
+                <div class="wrapper">
+                    <div class="header__logo">
+                        <img src="" alt="">
+                    </div>
+                    <div class="header__buttons">
+                        <?php // if($_SESSION) { ?>
+                        <a href="/index/signin" class="btn"><?php echo 'Sign In';?></a>
+                        <a href="/index/signup" class="btn"><?php echo 'Sign Up';?></a>
+                        <?php //} else{ ?>
+                        <span class="btn"><?php echo 'Hello User';?></span>
+                            <a href="/index/exit" class="btn"><?php echo 'Exit';?></a>
+                        <?php //} ?>
+                    </div>
+                </div>
+            </header>
+            <main class="main">
+                <?php include_once '../view/pages/' . $page . '_view.php'; ?>
+            </main>
+            <footer class="footer">
+                <div class="wrapper">
+                    <div class="footer-top">
+                        <div class="footer__logo">
+                            <img src="" alt="">
+                        </div>
+                        <address class="footer__address">
+                            <ul>
+                                <li>Name Surname</li>
+                                <li>Name Surname</li>
+                                <li>Name Surname</li>
+                                <li>Name Surname</li>
+                                <li>Name Surname</li>
+                                <li>Name Surname</li>
+                            </ul>
+                        </address>
+                    </div>
+                    <div class="footer-bottom">
+                        <div class="footer__copyright">
+                            <?php echo '© 2023, Level Up jan-2023';?>
+                        </div>
+                        <div class="footer__created">
+                            <?php echo 'Сайт створили: Mi4kire and Co';?>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
     </body>
-    <footer>
-        created by jan_team.
-    </footer>
 </html>
