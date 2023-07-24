80e8d40ca4bce9cfec685ed51cc94e3f9333e3c6
Vitalii-Tylnyi <vitaliy.tilniy@gmail.com>
2023-07-24 18:29:39 +0300

    BookingModel -> construct and find

--- app/models/BookingModel.php
+++ app/models/BookingModel.php
@@ -4,5 +4,28 @@ namespace models;
 
 class BookingModel
 {
+    protected \mysqli $db;
+
+    public function __construct()
+    {
+        $this->db = new \mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if($this->db->connect_errno != 0){
+            exit($this->db->connect_error);
+        }
+    }
+
+    /**
+     * @return array|null
+     * retrieves all reservations from a table
+     */
+    public function find() : ?array
+    {
+        $query = "SELECT * FROM reservations;";
+        $result = $this->db->query($query);
+        $this->checkResult($result);
+
+        return $result->fetch_assoc();
+    }
 
 }
